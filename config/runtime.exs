f1e2ad78adf2b6fb04dbf0cdfc532411c3ca0390
Paweł Świątkowski <katafrakt@vivaldi.net>
2022-04-09 16:02:18 +0200

    Prepare to release

--- config/runtime.exs
+++ config/runtime.exs
@@ -38,7 +38,8 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
-    secret_key_base: secret_key_base
+    secret_key_base: secret_key_base,
+    force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
   # ## Using releases
   #
