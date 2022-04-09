0dc3fa2cd3a10359fadd87018d96aa0269126816
Paweł Świątkowski <katafrakt@vivaldi.net>
2022-04-09 16:00:34 +0200

    Add releases

--- mix.exs
+++ mix.exs
@@ -6,7 +6,12 @@ defmodule Heroes.MixProject do
       apps_path: "apps",
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      releases: [
+        game_web: [
+          applications: [game_web: :permanent]
+        ]
+      ]
     ]
   end
 
